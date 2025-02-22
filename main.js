51c7f5edf3c07fc923500eb261160e7f2c3ef06a
Markusha <LegoAnimation2022@gmail.com>
2025-02-22 15:50:49 +0000

    1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+const token = '7917195254:AAGQzoiOrfGwY9mliUrXuCcrqrt4bjrGwHQ';
+const bot = new TelegramBot(token, {polling:true});
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name} ${msg.from.last_name}!\nДля начала работы <b>подтвердите, что вы ученик 7-М класса</b>.\n\nВопрос:\n<i><b>Назовите полное ФИО первого учителя русского языка в классе.</b></i>`, {parse_mode:'HTML'});
+
+    bot.on('message', (msg) => {
+        if (msg.text === "") 
+    });
+});
+
+console.log('> Successful start');
